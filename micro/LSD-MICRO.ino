b1f33b21e4e770ee82ad28dfea4cb5603b289040
djsounddog <djsounddog@gmail.com>
2020-11-03 17:56:07 +1100

    Refactored code to use class objects and inheritance. Now allows for variable length of sequence and brightness control as well as the ability to pause or stop the sequence.

--- micro/LSD-MICRO.ino
+++ micro/LSD-MICRO.ino
@@ -3,172 +3,261 @@
 // Fork of: AnthonyDavidAdams.com/lsd
 // Refactored by JFGilmore
 
-int set_duration();
-long light_program(long totalDuration);
-//void time_led(int duration);
-
 // Controller constants
-const int EFFECT_LED = 13;
-const int PLUS_BTN = 9;
-const int MINUS_BTN = 10;
-const int START_BTN = 11;
+const unsigned long INTERVAL = 5;
 
-const int TIME_1 = 3;
-const int TIME_2 = 4;
-const int TIME_3 = 5;
-const int TIME_4 = 6;
-const int TIME_5 = 7;
-const int TIME_6 = 8;
+const int EFFECT_LED = 11;
+const int PLUS_BTN = 8;
+const int MINUS_BTN = 9;
+const int START_BTN = 10;
 
-/******* WAVE VALUE CONSTANTS *******
-  Do not change except for experimental use
-*/
-const int b = 22;   // beta Hz
-const int a = 12;   // alpha/mu Hz
-const int d = 3;  // delta Hz
-const int t = 6;  // theta Hz
-const int g = 32;   // gamma Hz
+const int LED_METER_1 = 2;
+const int LED_METER_2 = 3;
+const int LED_METER_3 = 4;
+const int LED_METER_4 = 5;
+const int LED_METER_5 = 6;
+const int LED_METER_6 = 7;
 
-// [waveform][duration]
-const int sequence[][2] {
-  {b, 10} ,   // beta
-  {a, 10},    // alpha/mu
-  {d, 1},     // delta
-  {t, 1},     // theta
-  {g, 300},   // gamma
-  {g, 60},    // gamma
-  {t, 60},    // theta
-  {d, 60},    // delta
-  {a, 60},    // alpha/mu
-  {b, 60}     // beta
+class Display {  
+   public:
+     Display() {}
+     void init() {
+       Serial.begin(9600);
+       Serial.println();
+       Serial.println("DMTuino: Stroboscopic");
+       Serial.println("█▀▀▄ █▄░▄█ ███");
+       Serial.println("█░░█ █░█░█ ░█░");
+       Serial.println("█▄▄▀ █░░░█ ░█░");
+     }
+     void configure(unsigned long intervalLength) {
+       Serial.println("Select time [ " + (String)intervalLength + "mins -" + (String)intervalLength + "mins Start ]");
+     }
+     void sequenceStatus(unsigned long duration, int section, int frequency) {
+       String out = "Section: " + (String)section + " \t Frequency: " + (String)frequency + "hz \t Duration: " + (String)duration + "m";
+       Serial.println(out);
+     }
+     void start(int duration) {
+       String out = "SEQUENCE STARTED: " + (String)duration + " mins total\n";
+       Serial.println(out);
+     }
+     void stopSequence() {
+       Serial.println("STOPPED\n");
+     }
+     void pause() {
+       Serial.println("PAUSED\n");
+     }
+     void resume() {
+       Serial.println("RESUMED");
+     }
+ };
+
+class Button {
+  protected:
+    int pin;
+  public:
+    unsigned long duration;
+    const int noPress = 0;
+    const int shortPress = 1;
+    const int longPress = 2;
+    int state;
+    Button(int p) {
+      pin = p;
+    }
+    void init() {
+      pinMode(pin, INPUT);
+      state = noPress;
+    }
+    int check() {
+      duration = 0;
+      int reading = digitalRead(pin);
+      unsigned long longPressDuration = 500;
+
+      while (reading == HIGH) {
+        duration ++;
+        delay(1);
+        reading = digitalRead(pin);
+      }
+
+      if (duration >= longPressDuration) {
+        state = longPress;
+        return;
+      } else if (duration < longPressDuration && duration != noPress) {
+        state = shortPress;
+        return;
+      } else {
+        state = noPress;
+        return;
+      }
+    }
 };
 
+class Light {
+  protected:
+    int pin;
+  public:
+    const unsigned int minBrightness = 1;
+    const unsigned int maxBrightness = 255;
+    unsigned int brightness = maxBrightness;
+    Light(int p) {
+      pin = p;
+    }
+    void init() {
+      pinMode(pin, OUTPUT);
+      off();
+    }
+    ~Light() {
+      off();
+    }
+    void off() {
+      digitalWrite(pin, LOW);
+    }
+    void on(void) {
+      digitalWrite(pin, HIGH);
+    }
+    void on(unsigned int bright) {
+      analogWrite(pin, bright);
+    }
+};
+
+class EffectLight: public Light {
+  private:
+    unsigned long intervalLength; // base time unit in minutes.
+    unsigned long tDuration = 1;
+    Display disp = Display();
+    Button plus = Button(PLUS_BTN);
+    Button minus = Button(MINUS_BTN);
+    Button startStop = Button(START_BTN);
+    Light minLight = Light(LED_METER_1);
+    Light oneLight = Light(LED_METER_2);
+    Light twoLight = Light(LED_METER_3);
+    Light threeLight = Light(LED_METER_4);
+    Light fourLight = Light(LED_METER_5);
+    Light maxLight = Light(LED_METER_6);
+    unsigned int brightness = 255;
+    /******* WAVEFORM CONSTANTS *******
+    Only change for experimental purposes
+    */
+    const int b = 22; // beta Hz
+    const int a = 10; // alpha Hz => high intensity, simple visual hallucinations
+    const int d = 3;  // delta Hz => low intensity, complex visual hallucinations
+    const int t = 6;  // theta Hz
+    const int g = 32; // gamma Hz
+    // Sequence of opperating pattern [frequency(Hz)][duration(ms)]
+    const int sequence[6][2] = {
+      {b, 1},
+      {g, 1},
+      {a, 2},
+      {d, 2},
+      {g, 3},
+      {t, 1}
+    };
+
+  public:
+    EffectLight(int pin, unsigned long interval): Light(pin) {
+      intervalLength = interval;
+    }
+    void init() {
+      Light::init();
+      minLight.init();
+      oneLight.init();
+      twoLight.init();
+      threeLight.init();
+      fourLight.init();
+      maxLight.init();
+      plus.init();
+      minus.init();
+      startStop.init();
+      disp.init();
+    }
+    void configure() {
+      disp.configure(intervalLength);
+      while (startStop.state != startStop.shortPress)
+      {
+        // Set bightness & duration
+        plus.check();
+        minus.check();
+         if (plus.state == plus.longPress) {
+           brightness += 20;
+           brightness = (brightness > maxBrightness ? maxBrightness : brightness);
+           on(brightness);
+         } else if (minus.state == minus.longPress) {
+           brightness -= 20;
+           brightness = (brightness < minBrightness ? minBrightness : brightness);
+           on(brightness);
+         } else 
+        if (plus.state == plus.shortPress && tDuration < 6) {
+           tDuration ++;
+           digitalWrite(tDuration, HIGH);
+        } else if (minus.state == minus.shortPress && tDuration > 1) {
+           digitalWrite(tDuration, LOW);
+           tDuration --;
+        }
+        delay(50);
+        startStop.check();
+      }
+      return;
+    }
+
+    long runSequence(unsigned long duration, unsigned int bright) {
+      tDuration = duration;
+      brightness = bright;
+      runSequence();
+    }
+    long runSequence() {
+      disp.start(tDuration * intervalLength);
+      unsigned long startMillis;
+      unsigned long pauseMillis;
+      int hz;
+      int hzDelay;
+      unsigned long duration;
+
+      for (int repeat = tDuration; repeat > 0; repeat--) {
+        for (int i = 0; i < (sizeof sequence / sizeof sequence[0]); i++) {
+          hz = sequence[i][0];
+          duration = sequence[i][1]; // in seconds
+          hzDelay = 1000 / hz;
+          startMillis = millis();
+
+          disp.sequenceStatus(duration, i, hz);
+          
+          while((millis() - startMillis) < duration * 60000) {
+            on(brightness);
+            delay(hzDelay);
+            off();
+            delay(hzDelay);
+
+            startStop.check();
+            if (startStop.state == startStop.shortPress) {
+              // Pause
+              pauseMillis = millis();
+              disp.pause();
+              do {
+                delay(50);
+                startStop.check();
+              } while (startStop.state == startStop.noPress);
+              disp.resume();
+              startMillis -= (millis() - pauseMillis);
+            }
+            if (startStop.state == startStop.longPress) {
+              // Stop
+              disp.stopSequence();
+              startStop.state = startStop.noPress;
+              return;
+            }
+          }
+        }
+        digitalWrite(tDuration, LOW);
+      }
+    }
+};
+
+EffectLight effectLed(EFFECT_LED, INTERVAL);
+
 void setup() {
-  pinMode(EFFECT_LED, OUTPUT);
-  pinMode(PLUS_BTN, INPUT);
-  pinMode(MINUS_BTN, INPUT);
-  pinMode(START_BTN, INPUT);
-
-  pinMode(TIME_1, OUTPUT);
-  pinMode(TIME_2, OUTPUT);
-  pinMode(TIME_3, OUTPUT);
-  pinMode(TIME_4, OUTPUT);
-  pinMode(TIME_5, OUTPUT);
-  pinMode(TIME_6, OUTPUT);
-  
-  Serial.begin(9600);
-
-  Serial.println("DMTuino: Stroboscopic");
-  Serial.println();
-  Serial.println("█▀▀▄ █▄    ▄█ ███");
-  Serial.println("█        █ █    █    █     █    ");
-  Serial.println("█▄▄▀ █            █     █    ");
+  effectLed.init();
 }
 
 void loop() {
-  const int t_base = 5; // base time unit in minutes.
-  String str_out = "Select time [ " + (String)t_base + "mins -" + (String)t_base + "mins Start ]";
-  Serial.println(str_out);
-
-  long duration = set_duration();
-  light_program(duration * t_base);
+  effectLed.configure();
+  effectLed.runSequence();
 }
-
-int set_duration() {
-  int duration = 1;
-  int minus, minusPrev, plus, plusPrev, strt = LOW;
-  for(int i = TIME_1; i < TIME_6; i++) {
-    digitalWrite(i, LOW);
-  }
-  digitalWrite(TIME_1, HIGH);
-  
-  while (strt == LOW) {
-    minus = digitalRead(MINUS_BTN);
-    plus = digitalRead(PLUS_BTN);
-    strt = digitalRead(START_BTN);
-
-    if (minus != minusPrev) {
-      if (minus == HIGH && duration > 1) {
-        duration = duration - 1;
-        digitalWrite(duration + TIME_1, LOW);
-      } 
-    } else if (plus != plusPrev) {
-      if (plus == HIGH && duration < 6) {
-        digitalWrite(duration + TIME_1, HIGH);
-        duration = duration + 1;
-      }
-    }
-    minusPrev = minus;
-    plusPrev = plus;
-    // Debounce
-    delay(50);
-  }
-
-  return(duration);
-}
-
-long light_program(long totalDuration) {
-  unsigned long startMillis = millis();
-  float hz = b ;
-  int hzDelay = 33;
-  int stp, stpPrev = LOW;
-  int duration = 0;
-  totalDuration = totalDuration * 6000; // time in mins to ms
-
-  for (int i = 0; i < (sizeof sequence / sizeof sequence[0]); i++) {
-    duration = sequence[i][1]; // in seconds
-    totalDuration = totalDuration + duration;
-  }
-  
-  Serial.println("SEQUENCE STARTED:");
-  Serial.print(totalDuration / 6000);
-  Serial.println(" Minutes total");
-  Serial.println();
-
-  for (int i = 0; i < (sizeof sequence / sizeof sequence[0]); i++) {
-
-    hz = sequence[i][0];
-    duration = sequence[i][1]; // in seconds
-    
-    hzDelay =  (1 / hz) * 1000;
-    
-    startMillis = millis();
-    
-    Serial.print("Section: ");
-    Serial.print(i);
-    Serial.print("\t Frequency: ");
-    Serial.print(hz);
-    Serial.print("hz \tDelay: ");
-    Serial.print(hzDelay);
-    Serial.print("\t Duration: ");
-    Serial.print(" ");
-    Serial.print(duration);
-    Serial.print(" Seconds \t Actual: ");
-    
-    while(((millis() - startMillis)/1000 )< duration) {
-      digitalWrite(EFFECT_LED, HIGH);
-      delay(hzDelay);
-      digitalWrite(EFFECT_LED, LOW);
-      delay(hzDelay);
-
-      stp = digitalRead(START_BTN);
-      if (stp == HIGH) {
-        Serial.println("CANCELLED!");
-        Serial.println("Resetting...");
-        delay(2000);
-        return;
-      }
-      
-    }
-    Serial.print((millis() - startMillis)/1000);
-    Serial.println(" Seconds");
-
-  }
-  Serial.println("SEQUENCE END");
-  Serial.println();
-  return;
-}
-//
-//void time_led(int duration) {
-//  
-//}
